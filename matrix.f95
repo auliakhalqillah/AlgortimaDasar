3d7fb0527428a5d7c1f972a96c292fa7b9bd83d1
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2021-01-01 16:06:16 +0700

    Add files via upload

---
+++ matrix.f95
@@ -0,0 +1,29 @@
+program matrixsederhana
+    implicit none
+
+    integer :: i,j
+    real :: a(3,2)
+
+    ! Defenisikan nilai dalam matrix
+    a(1,1) = 1
+    a(1,2) = 2
+    a(2,1) = 4
+    a(2,2) = 5
+    a(3,1) = 7
+    a(3,2) = 8
+
+    ! Tampilkan matrix
+    do i = 1,3
+        do j = 1,2
+            write(*,*) a(i,j)
+        end do
+    end do
+
+    write(*,*)
+
+    ! Cara lain menampilkan matrix
+    do i = 1,3
+        write(*,*) (a(i,j), j=1,2)
+    end do
+    
+end program matrixsederhana
